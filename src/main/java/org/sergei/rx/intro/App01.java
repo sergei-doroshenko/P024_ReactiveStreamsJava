28da4a6fa26eb7018869e41c0e353c9386569d9f
sergei.doroshenko <6823298@gmail.com>
2016-08-31 12:45:35 +0300

    Initial commit.

---
+++ src/main/java/org/sergei/rx/intro/App01.java
@@ -0,0 +1,10 @@
+package org.sergei.rx.intro;
+
+/**
+ * Created by Sergei_Doroshenko on 8/31/2016.
+ */
+public class App01 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
