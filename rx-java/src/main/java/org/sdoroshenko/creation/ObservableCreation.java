f6715b8b1652f59a938cbf17953f4b8e577cbf40
sergei-doroshenko <6823298@gmail.com>
2019-08-11 19:58:57 +0300

    RxJava : Observable creation

--- rx-java/src/main/java/org/sdoroshenko/creation/ObservableCreation.java
+++ rx-java/src/main/java/org/sdoroshenko/creation/ObservableCreation.java
@@ -2,14 +2,29 @@ package org.sdoroshenko.creation;
 
 import rx.Emitter;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
+
+import java.util.concurrent.CountDownLatch;
 
 public class ObservableCreation {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Observable<Integer> observable = Observable.create(new MyAction(), Emitter.BackpressureMode.NONE);
         observable.subscribe(i -> System.out.println("Got: " + i));
+
+        CountDownLatch latch = new CountDownLatch(4);
+        Observable.create(new MyOnSubscribe(4))
+                .observeOn(Schedulers.newThread())
+                .doOnEach(i -> latch.countDown())
+                .subscribe(
+                        i -> System.out.println("Got 2: " + i),
+                        error -> System.out.println("Error: " + error.getMessage())
+                );
+
+        latch.await();
     }
 
     public static class MyAction implements Action1<Emitter<Integer>> {
